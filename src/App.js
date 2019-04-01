8cdf6e2dda2cf3067a75744c67abf35af9952934
Alex Krivtsun <krivtsuna@mail.ru>
2019-04-01 18:27:32 +0300

    change BrowserRouter as HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
 import ListPage from "./components/list/ListPage";
 
